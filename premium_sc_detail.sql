3e8a22e788e7590b65e27525109c1733a36c2b3d
esterTion <lcz970@126.com>
2018-04-03 12:33:59 +0800

    10025450 2017/04/14 20:20

--- premium_sc_detail.sql
+++ premium_sc_detail.sql
@@ -2,5 +2,6 @@ CREATE TABLE 'premium_sc_detail' ('item_id' INTEGER NOT NULL, 'jewel_shop_id' IN
 INSERT INTO `premium_sc_detail` VALUES (/*item_id*/80001, /*jewel_shop_id*/1, /*use_period_day*/7, /*all_user_use_end_date*/"");
 INSERT INTO `premium_sc_detail` VALUES (/*item_id*/80002, /*jewel_shop_id*/2, /*use_period_day*/7, /*all_user_use_end_date*/"");
 INSERT INTO `premium_sc_detail` VALUES (/*item_id*/80003, /*jewel_shop_id*/3, /*use_period_day*/7, /*all_user_use_end_date*/"");
+INSERT INTO `premium_sc_detail` VALUES (/*item_id*/80004, /*jewel_shop_id*/6, /*use_period_day*/7, /*all_user_use_end_date*/"");
 INSERT INTO `premium_sc_detail` VALUES (/*item_id*/81001, /*jewel_shop_id*/4, /*use_period_day*/0, /*all_user_use_end_date*/"2017-02-28 23:59:59");
 INSERT INTO `premium_sc_detail` VALUES (/*item_id*/81002, /*jewel_shop_id*/5, /*use_period_day*/0, /*all_user_use_end_date*/"2017-02-28 23:59:59");
