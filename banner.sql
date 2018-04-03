81967058cdcb2c6a44698838f53811fe6532d88d
esterTion <lcz970@126.com>
2018-04-03 12:29:31 +0800

    10017000 2016/07/13 20:26

--- banner.sql
+++ banner.sql
@@ -85,3 +85,7 @@ INSERT INTO `banner` VALUES (/*banner_id*/83, /*type*/2, /*img_id*/3009, /*trans
 INSERT INTO `banner` VALUES (/*banner_id*/84, /*type*/1, /*img_id*/0, /*transition*/0, /*page_index*/0, /*order_num*/2, /*start_date*/"2016-06-30 15:00:00", /*end_date*/"2016-07-10 14:59:59");
 INSERT INTO `banner` VALUES (/*banner_id*/85, /*type*/2, /*img_id*/2008, /*transition*/0, /*page_index*/0, /*order_num*/3, /*start_date*/"2015-04-01 15:00:00", /*end_date*/"2030-04-01 14:00:00");
 INSERT INTO `banner` VALUES (/*banner_id*/86, /*type*/1, /*img_id*/0, /*transition*/0, /*page_index*/0, /*order_num*/2, /*start_date*/"2016-07-10 15:00:00", /*end_date*/"2016-07-16 14:59:59");
+INSERT INTO `banner` VALUES (/*banner_id*/87, /*type*/1, /*img_id*/0, /*transition*/0, /*page_index*/0, /*order_num*/2, /*start_date*/"2016-07-16 15:00:00", /*end_date*/"2016-07-17 14:59:59");
+INSERT INTO `banner` VALUES (/*banner_id*/88, /*type*/1, /*img_id*/0, /*transition*/0, /*page_index*/0, /*order_num*/2, /*start_date*/"2016-07-17 15:00:00", /*end_date*/"2016-07-18 14:59:59");
+INSERT INTO `banner` VALUES (/*banner_id*/89, /*type*/1, /*img_id*/0, /*transition*/0, /*page_index*/0, /*order_num*/2, /*start_date*/"2016-07-18 15:00:00", /*end_date*/"2016-07-19 14:59:59");
+INSERT INTO `banner` VALUES (/*banner_id*/91, /*type*/2, /*img_id*/1011, /*transition*/0, /*page_index*/0, /*order_num*/3, /*start_date*/"2015-04-01 15:00:00", /*end_date*/"2030-04-01 14:00:00");
