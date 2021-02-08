0b9639d2c1f24b0263c32a25a7914d51523b734e
esterTion-serveraccount <estertion@estertion.win>
2021-02-08 14:00:48 +0800

    10081600 - new event シンデレラキャラバン

--- asset_policy_setting.sql
+++ asset_policy_setting.sql
@@ -1,2 +1,3 @@
 CREATE TABLE 'asset_policy_setting' ('id' INTEGER NOT NULL, 'asset_name' TEXT NOT NULL, 'release_date' TEXT NOT NULL, PRIMARY KEY('id'));
-INSERT INTO `asset_policy_setting` VALUES (/*id*/1, /*asset_name*/"m/live/high/2drich2037.usm", /*release_date*/"2021-02-08 11:59:00");
+INSERT INTO `asset_policy_setting` VALUES (/*id*/1, /*asset_name*/"cappuccino_thumbnail_020039.unity3d", /*release_date*/"2021-02-09 14:59:00");
+INSERT INTO `asset_policy_setting` VALUES (/*id*/2, /*asset_name*/"cappuccino_thumbnail_top_020039.unity3d", /*release_date*/"2021-02-09 14:59:00");
