8279c9ea50c6270222fe08b07e73571cff44ae46
esterTion-serveraccount <estertion@estertion.win>
2022-06-10 23:00:43 +0800

    10096850

--- cenere_data.sql
+++ cenere_data.sql
@@ -1,2 +1,2 @@
 CREATE TABLE 'cenere_data' ('id' INTEGER NOT NULL, 'name' TEXT NOT NULL, 'bonus_ids' TEXT NOT NULL, PRIMARY KEY('id'));
-INSERT INTO `cenere_data` VALUES (/*id*/1001, /*name*/"プレミアムパス", /*bonus_ids*/"101,102,103,104,105,106");
+INSERT INTO `cenere_data` VALUES (/*id*/1001, /*name*/"プレミアムパス", /*bonus_ids*/"101,102,103,104,105,106,107,108");
