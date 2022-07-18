d4c49cc05a56f06fd3d6ae7fe0f1e5cfd38cbb1e
esterTion-serveraccount <estertion@estertion.win>
2022-07-18 14:00:43 +0800

    10097850 - new event サマーサイダー

--- banner.sql
+++ banner.sql
@@ -1596,3 +1596,4 @@ INSERT INTO `banner` VALUES (/*banner_id*/1618, /*type*/2, /*img_id*/6028, /*tra
 INSERT INTO `banner` VALUES (/*banner_id*/1619, /*type*/1, /*img_id*/30780, /*transition*/0, /*page_index*/30780, /*order_num*/4, /*start_date*/"2022-07-13 15:00:00", /*end_date*/"2022-07-16 14:59:59");
 INSERT INTO `banner` VALUES (/*banner_id*/1620, /*type*/1, /*img_id*/30781, /*transition*/0, /*page_index*/30781, /*order_num*/5, /*start_date*/"2022-07-13 15:00:00", /*end_date*/"2022-07-16 14:59:59");
 INSERT INTO `banner` VALUES (/*banner_id*/1621, /*type*/1, /*img_id*/30782, /*transition*/0, /*page_index*/30782, /*order_num*/5, /*start_date*/"2022-07-16 15:00:00", /*end_date*/"2022-07-19 14:59:59");
+INSERT INTO `banner` VALUES (/*banner_id*/1627, /*type*/2, /*img_id*/1080, /*transition*/0, /*page_index*/0, /*order_num*/4, /*start_date*/"2015-04-01 15:00:00", /*end_date*/"2030-04-01 14:00:00");
