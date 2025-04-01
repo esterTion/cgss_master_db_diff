7d9c3e838b651f8ace9b68b06a11978493629353
esterTion-serveraccount <estertion@estertion.win>
2025-04-01 23:01:10 +0800

    10128800

--- arcade_cpu_setting.sql
+++ arcade_cpu_setting.sql
@@ -1,5 +1,5 @@
 CREATE TABLE 'arcade_cpu_setting' ('minigame_id' INTEGER NOT NULL, 'player_name' TEXT NOT NULL, 'level_chara_id' INTEGER NOT NULL, PRIMARY KEY('minigame_id'));
-INSERT INTO `arcade_cpu_setting` VALUES (/*minigame_id*/10, /*player_name*/"シャドウぴにゃこら太", /*level_chara_id*/370);
+INSERT INTO `arcade_cpu_setting` VALUES (/*minigame_id*/10, /*player_name*/"シャドウ冴島清美", /*level_chara_id*/370);
 INSERT INTO `arcade_cpu_setting` VALUES (/*minigame_id*/11, /*player_name*/"シャドウ砂塚あきら", /*level_chara_id*/375);
 INSERT INTO `arcade_cpu_setting` VALUES (/*minigame_id*/12, /*player_name*/"シャドウ双葉杏", /*level_chara_id*/192);
 INSERT INTO `arcade_cpu_setting` VALUES (/*minigame_id*/13, /*player_name*/"シャドウ三好紗南", /*level_chara_id*/364);
