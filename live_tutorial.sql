ee934f8a7864c45b687a6101c3c21ea916cfccc5
esterTion-serveraccount <estertion@estertion.win>
2025-04-26 20:01:12 +0800

    10129400

--- live_tutorial.sql
+++ live_tutorial.sql
@@ -1,2 +1,2 @@
 CREATE TABLE 'live_tutorial' ('id' INTEGER NOT NULL, 'start_date' TEXT NOT NULL, 'end_date' TEXT NOT NULL, PRIMARY KEY('id'));
-INSERT INTO `live_tutorial` VALUES (/*id*/120, /*start_date*/"2025-04-15 00:00:00", /*end_date*/"2025-04-27 23:59:59");
+INSERT INTO `live_tutorial` VALUES (/*id*/121, /*start_date*/"2025-04-28 00:00:00", /*end_date*/"2025-05-07 23:59:59");
