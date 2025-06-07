d6f78ba64c16dc830ad03bab284715d91459c171
esterTion-serveraccount <estertion@estertion.win>
2025-06-07 20:01:08 +0800

    10130300

--- banner.sql
+++ banner.sql
@@ -2478,5 +2478,7 @@ INSERT INTO `banner` VALUES (/*banner_id*/2513, /*type*/17, /*img_id*/2, /*trans
 INSERT INTO `banner` VALUES (/*banner_id*/2514, /*type*/17, /*img_id*/3, /*transition*/1, /*page_index*/0, /*order_num*/50, /*start_date*/"2025-06-17 00:00:00", /*end_date*/"2025-07-31 23:59:59");
 INSERT INTO `banner` VALUES (/*banner_id*/2515, /*type*/1, /*img_id*/31100, /*transition*/0, /*page_index*/31100, /*order_num*/2, /*start_date*/"2025-05-29 15:00:00", /*end_date*/"2025-06-06 14:59:59");
 INSERT INTO `banner` VALUES (/*banner_id*/2516, /*type*/1, /*img_id*/80116, /*transition*/2, /*page_index*/152, /*order_num*/16, /*start_date*/"2015-04-01 15:00:00", /*end_date*/"2030-04-01 14:00:00");
-INSERT INTO `banner` VALUES (/*banner_id*/2517, /*type*/1, /*img_id*/31101, /*transition*/0, /*page_index*/31101, /*order_num*/1, /*start_date*/"2025-06-06 15:00:00", /*end_date*/"2025-06-16 14:59:59");
-INSERT INTO `banner` VALUES (/*banner_id*/2518, /*type*/1, /*img_id*/31102, /*transition*/0, /*page_index*/31102, /*order_num*/2, /*start_date*/"2025-06-06 15:00:00", /*end_date*/"2025-06-16 14:59:59");
+INSERT INTO `banner` VALUES (/*banner_id*/2517, /*type*/1, /*img_id*/31101, /*transition*/0, /*page_index*/31101, /*order_num*/3, /*start_date*/"2025-06-06 15:00:00", /*end_date*/"2025-06-16 14:59:59");
+INSERT INTO `banner` VALUES (/*banner_id*/2518, /*type*/1, /*img_id*/31102, /*transition*/0, /*page_index*/31102, /*order_num*/4, /*start_date*/"2025-06-06 15:00:00", /*end_date*/"2025-06-16 14:59:59");
+INSERT INTO `banner` VALUES (/*banner_id*/2519, /*type*/1, /*img_id*/60422, /*transition*/3, /*page_index*/60422, /*order_num*/2, /*start_date*/"2025-06-07 21:00:00", /*end_date*/"2025-06-17 23:59:59");
+INSERT INTO `banner` VALUES (/*banner_id*/2520, /*type*/1, /*img_id*/60423, /*transition*/3, /*page_index*/60423, /*order_num*/1, /*start_date*/"2025-06-08 19:00:00", /*end_date*/"2025-06-17 23:59:59");
