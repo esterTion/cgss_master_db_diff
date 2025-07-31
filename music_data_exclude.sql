55901350aee527e13bf915323ae920160199fea1
esterTion-serveraccount <estertion@estertion.win>
2025-07-31 23:01:03 +0800

    10131900

--- music_data_exclude.sql
+++ music_data_exclude.sql
@@ -2,3 +2,7 @@ CREATE TABLE 'music_data_exclude' ('id' INTEGER NOT NULL, 'music_data_id' INTEGE
 INSERT INTO `music_data_exclude` VALUES (/*id*/1, /*music_data_id*/0, /*start_date*/"2099-03-30 15:00:00", /*end_date*/"2099-04-01 15:00:00");
 INSERT INTO `music_data_exclude` VALUES (/*id*/2, /*music_data_id*/7015, /*start_date*/"2015-03-01 15:00:00", /*end_date*/"2024-05-01 14:59:59");
 INSERT INTO `music_data_exclude` VALUES (/*id*/3, /*music_data_id*/7019, /*start_date*/"2015-03-01 15:00:00", /*end_date*/"2024-09-12 14:59:59");
+INSERT INTO `music_data_exclude` VALUES (/*id*/4, /*music_data_id*/8062, /*start_date*/"2025-08-01 00:00:00", /*end_date*/"2099-04-01 15:00:00");
+INSERT INTO `music_data_exclude` VALUES (/*id*/5, /*music_data_id*/8063, /*start_date*/"2025-08-01 00:00:00", /*end_date*/"2099-04-01 15:00:00");
+INSERT INTO `music_data_exclude` VALUES (/*id*/6, /*music_data_id*/8064, /*start_date*/"2025-08-01 00:00:00", /*end_date*/"2099-04-01 15:00:00");
+INSERT INTO `music_data_exclude` VALUES (/*id*/7, /*music_data_id*/8065, /*start_date*/"2025-08-01 00:00:00", /*end_date*/"2099-04-01 15:00:00");
